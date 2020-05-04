e9c890fdcbd4b47123043ea917b09d8a8682a8d5
RahulJadhav26 <48377282+RahulJadhav26@users.noreply.github.com>
2020-05-04 15:48:09 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -12,14 +12,14 @@ app.use(cors())
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyALe4A4s27xcLjVqF0ONRe_0kvR_3NGUKY",
-    authDomain: "port-66459.firebaseapp.com",
-    databaseURL: "https://port-66459.firebaseio.com",
-    projectId: "port-66459",
-    storageBucket: "port-66459.appspot.com",
-    messagingSenderId: "418283710045",
-    appId: "1:418283710045:web:c4ce523c5c0bfed3704fb4",
-    measurementId: "G-5VX0DER8QN"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 
   firebase.initializeApp(firebaseConfig)
